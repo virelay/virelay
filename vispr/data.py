369cb71b5103adff5ad1d5647357c26c811b5407
chrstphr <chrstphr@posteo.eu>
2019-07-05 16:13:53 +0200

    refactoring and update to standard hdf5 format

--- vispr/data.py
+++ vispr/data.py
@@ -1,6 +1,5 @@
 import os
 
-import h5py
 import numpy as np
 from PIL import Image
 
@@ -14,40 +13,19 @@ def center_crop(img, output_size):
     j = int(round((w - tw) / 2.))
     return crop(img, i, j, th, tw)
 
-def get_attrib(atpath, cat):
-    fname = os.path.join(atpath, 'train_set.{}.dtd.h5'.format(cat))
-    with h5py.File(fname, 'r') as fp:
-        attrib = fp['attribution'][:].mean(1)
-        pred = fp['prediction'][:]
-    attrib /= attrib.max(0)
-    return attrib[:, ::-1], pred
-
-def get_clu(anpath, cat):
-    clupath = os.path.join(anpath, '{}.clu.h5'.format(cat))
-    with h5py.File(clupath, 'r') as fp:
-        kcluster = fp['kcluster'][:]
-        label = fp['label'][:]
-    return kcluster, label
-
-def get_ew(anpath, cat):
-    clupath = os.path.join(anpath, '{}.emb.h5'.format(cat))
-    with h5py.File(clupath, 'r') as fp:
-        ew = fp['ew'][:]
-    return ew
-
-def get_tsne(anpath, cat):
-    tsnpath = os.path.join(anpath, '{}.tsn.h5'.format(cat))
-    with h5py.File(tsnpath, 'r') as fp:
-        tsne = fp['tsne'][:]
-    return tsne
-
 class OrigImage(object):
-    def __init__(self, inpath, cat):
-        self._fpath = os.path.join(inpath, '{}.tar'.format(cat))
-        self._items = sorted([fname for fname in os.listdir(self._fpath) if fname[-5:] == '.JPEG'])
+    def __init__(self, inpath):
+        dmatch = re.compile(r'n\d{8}\.tar').fullmatch
+        fmatch = re.compile(r'n\d{8}_\d+\.JPEG').fullmatch
+        self._fpath = inpath
+        self._index = list(
+            os.path.join(dname, fname)
+            for dname in sorted(filter(dmatch, os.listdir(inpath)))
+            for fname in sorted(filter(fmatch, os.listdir(dname)))
+        )
 
     def _load_index(self, key):
-        img = Image.open(os.path.join(self._fpath, self._items[key]))
+        img = Image.open(self._index[key])
         img = center_crop(img, (224, 224))
         img = img.convert('RGB')
         img.putalpha(255)
--- vispr/handler.py
+++ vispr/handler.py
@@ -1,10 +1,9 @@
 import os
 import json
-
-import numpy as np
-
 from argparse import Namespace
 
+import h5py
+import numpy as np
 from bokeh.layouts import row, column, gridplot
 from bokeh.models import ColumnDataSource, CDSView, IndexFilter, DataTable, TableColumn, Range1d, LinearColorMapper
 from bokeh.events import Reset
@@ -13,119 +12,157 @@ from bokeh.plotting import figure, curdoc
 from bokeh.models.widgets import RadioButtonGroup, Select, Slider
 from bokeh.palettes import brewer, d3
 
-from .data import center_crop, get_attrib, get_clu, get_ew, get_tsne, OrigImage
+from .data import OrigImage
 
-def modify_doc(doc, inpath, atpath, anpath, wordmap):
-    cats = [fname[:-7] for fname in os.listdir(anpath) if fname[-7:] == '.clu.h5']
-    with open(wordmap, 'r') as fd:
+def modify_doc(doc, original_path, attribution_path, analysis_path, wordmap_path, wnid_path):
+    # load all analysis category names
+    with h5py.File(analysis_path, 'r') as fd:
+        categories = list(fd)
+    # load mapping from label number to wnid
+    with open(wnid_path, 'r') as fp:
+        wnids = fp.read().split('\n')[:-1]
+    # load mapping from wnid to description
+    with open(wordmap_path, 'r') as fd:
         words = json.load(fd)
+    mapwnid = lambda x: words.get(x, 'UNKNOWN')
 
+    # loader for original input images
+    original_loader = OrigImage(original_path, new)
+
+    # Namespace to store data references
     data = Namespace()
-    data.cat = cats[0]
-    data.lab = 0
+    data.sel = Namespace()
+    data.sel.cat = categories[0]
+    data.sel.clu = None
+    data.sel.vis = None
 
+    # load data for a new category
     def update_cat(attr, old, new):
-        data.kcluster, data.label = get_clu(anpath, new)
-        tsne = get_tsne(anpath, new)
-        x, y = tsne.T
-        ew = get_ew(anpath, new)
-        data.orig = OrigImage(inpath, new)
-        data.attrib, pred = get_attrib(atpath, new)
-        source.data.update({'i': range(len(x)), 'x': x, 'y': y, 'label': data.label[data.lab],
-                            'pred': cats.index(new) == pred})
-        inds = source.selected.indices[:num] if len(source.selected.indices) else list(range(num))
-        imattrib = list(data.attrib[inds])
-        imorig = list(data.orig[inds])
-        imsource.data.update({
-            'attrib': imattrib,
-            'orig': imorig,
-            'x': (np.arange(len(inds), dtype=int)*wid)%maxwid,
-            'y': (np.arange(len(inds), dtype=int)*wid)//maxwid*hei,
-        })
-        ewsource.data.update({
-            'x': range(len(ew)),
-            'y': ew[::-1],
-        })
-        data.cat = new
-        source.selected.indices = []
+        data.sel.cat = new
+        # analysis data
+        with h5py.File(analysis_path, 'r') as fd:
+            fd = fd[data.sel.cat]
+            data.cluster = {key: val[:] for key in fd['cluster'].items() }
+            data.visual = {key: val[:] for key in fd['visualization'].items() }
+            data.eigenvalue = fd['eigenvalue'][:]
+            data.index = fd['index'][:]
+        visual_x, visual_y = data.visualization[data.sel.vis].T
 
-    #cmap = ['red', 'green', 'blue', 'orange', 'cyan', 'magenta', 'black', 'violet']
+        # attribution data
+        with h5py.file(attribution_path, 'r') as fd:
+            data.attribution = fd['attribution'][data.index, :]
+            data.prediction = fd['prediction'][data.index, :]
+
+        # updated selected values
+        if data.sel.clu not in data.cluster:
+            data.sel.clu = data.cluster.keys()[0]
+        if data.sel.vis not in data.visualization:
+            data.sel.vis = data.visualization.keys()[0]
+
+        # label descriptions for each sample prediction
+        predword = [mapword(wnids[label_id]) for label_id in prediction.argmax(1)]
+
+        # update sample source with new category data
+        sample_src.data.update({'i': range(len(x)), 'x': visual_x, 'y': visual_y, 'cluster': data.cluster[data.sel.clu],
+                                'prediction': predword})
+        # unselect everything
+        sample_src.selected.indices = []
+
+        # decide which images to show in image_src
+        inds = list(range(num))
+        image_src.data.update({
+            'attribution': list(data.attribution[data.index[inds]]),
+            'original'   : list(data.original_loader[data.index[inds]]),
+            'x'          : (np.arange(len(inds), dtype=int)*wid)%maxwid,
+            'y'          : (np.arange(len(inds), dtype=int)*wid)//maxwid*hei,
+        })
+
+        # update eigenvalue plot
+        eigval_src.data.update({
+            'x': range(len(eigenvalue)),
+            'y': eigenvalue[::-1],
+        })
+
+
+    # various plotting constants
     cmap = d3['Category20'][12]
+    #cmap = ['red', 'green', 'blue', 'orange', 'cyan', 'magenta', 'black', 'violet']
+
     wid, hei = (224, 224)
     maxwid = 4*224
     maxhei = 4*224
     num = 16
 
-    source = ColumnDataSource({'i': [], 'x': [], 'y': [], 'label': [], 'pred': []})
-    imsource = ColumnDataSource({'attrib': [], 'orig': [], 'x': [], 'y': []})
-    ewsource = ColumnDataSource({'x': [], 'y': []})
+    # initialize sources
+    sample_src = ColumnDataSource({'i': [], 'x': [], 'y': [], 'cluster': [], 'prediction': []})
+    image_src = ColumnDataSource({'attribution': [], 'original': [], 'x': [], 'y': []})
+    eigval_src = ColumnDataSource({'x': [], 'y': []})
 
-    update_cat(None, data.cat, data.cat)
+    update_cat(None, data.sel.cat, data.sel.cat)
 
     TOOLS="pan,wheel_zoom,box_select,lasso_select,reset"
 
-    fbcol = "#3A5785"
-    ewp = figure(tools=[], plot_width=200, plot_height=800, min_border=1, min_border_left=1,
-                 toolbar_location="above", title="Eigenvalues")
-    r = ewp.scatter('x', 'y', source=ewsource, size=4, color=fbcol)
+    # === Eigenvalue Figure ===
+    eigval_fig  = figure(tools=[], plot_width=200, plot_height=800, min_border=1, min_border_left=1,
+                        toolbar_location="above", title="Eigenvalues")
+    eigval_rend = eigval_fig.scatter('x', 'y', source=eigval_src, size=4, color="#3A5785")
 
-    # create the scatter plot
-    p = figure(tools=TOOLS, plot_width=600, plot_height=800, min_border=1, min_border_left=1,
-               toolbar_location="above", x_axis_location=None, y_axis_location=None,
-               title="TSNE")
+    # === Cluster Visualization Figure (e.g. TSNE) ===
+    visual_fig  = figure(tools=TOOLS, plot_width=600, plot_height=800, min_border=1, min_border_left=1,
+                        toolbar_location="above", x_axis_location=None, y_axis_location=None,
+                        title="Visualization")
+    visual_cmap = linear_cmap('cluster', cmap, 0, len(cmap) - 1)
+    visual_rend = visual_fig.scatter('x', 'y', source=sample_src, size=6, color=visual_cmap,
+                                    nonselection_alpha=0.2, nonselection_color=visual_cmap)
 
-    col = linear_cmap('label', cmap, 0, len(cmap) - 1)
-    r = p.scatter('x', 'y', source=source, size=6, color=col,
-                  nonselection_alpha=0.2, nonselection_color=col)
-
-    columns = [
-        TableColumn(field='label', title='label'),
-        #TableColumn(field='pred', title='pred'),
+    # === Table of selected Samples ===
+    sample_columns = [
+        TableColumn(field='cluster', title='cluster'),
+        TableColumn(field='prediction', title='prediction'),
     ]
-    stats = DataTable(source=source, columns=columns, width=150, height=800)
+    sample_table = DataTable(source=sample_src, columns=sample_columns, width=150, height=800)
 
-    pi = figure(tools=[], plot_width=800, plot_height=800, min_border=10, min_border_left=10,
-                toolbar_location=None, x_axis_location=None, y_axis_location=None,
-                title="Images", x_range=Range1d(start=0, end=maxwid), y_range=Range1d(start=0, end=maxhei))
+    # === Figure containing original images and attributions ===
+    image_fig = figure(tools=[], plot_width=800, plot_height=800, min_border=10, min_border_left=10,
+                       toolbar_location=None, x_axis_location=None, y_axis_location=None,
+                       title="Images", x_range=Range1d(start=0, end=maxwid), y_range=Range1d(start=0, end=maxhei))
+    attribution_rend = pi.image('attribution', x='x', y='y', dw=wid, dh=hei, source=image_src, palette='Magma256', global_alpha=0.0)
+    original_rend    = pi.image_rgba('original', x='x', y='y', dw=wid, dh=hei, source=image_src, global_alpha=1.0)
 
-    imattr = pi.image('attrib', x='x', y='y', dw=wid, dh=hei, source=imsource, palette='Magma256', global_alpha=0.0)
-    imorig = pi.image_rgba('orig', x='x', y='y', dw=wid, dh=hei, source=imsource, global_alpha=1.0)
+    # === Widgets ==
+    category_select = Select(value=data.sel.cat, options=[(k, '{} ({})'.format(words[k], k)) for k in categories])
+    cluster_select  = Select(value=data.sel.clu, options=[k for k in data.cluster], width=200)
+    alpha_slider = Slider(start=0.0, end=1.0, step=0.05, value=0.0, width=100)
 
-    select = Select(value=data.cat, options=[(k, '%s (%s)'%(words[k], k)) for k in cats])
-    radio = RadioButtonGroup(labels=["%d"%i for i in data.kcluster], active=data.lab, width=400)
-    slider = Slider(start=0.0, end=1.0, step=0.05, value=0.0, width=100)
-
-    top = row(select, radio, slider)
-    bottom = row(ewp, stats, p, pi)
+    # === Document Layout ===
+    top = row(category_select, cluster_select, alpha_slider)
+    bottom = row(eigval_fig, sample_table, visual_fig, pi)
     layout = column(top, bottom)
-
     doc.add_root(layout)
     doc.title = "Sprincl TSNE"
 
-    empty = np.zeros_like(data.attrib[0])
-
     def update_selection(attr, old, new):
-        stats.view = CDSView(source=source, filters=[IndexFilter(new)])
-        inds = source.selected.indices[:num] if len(source.selected.indices) else list(range(num))
-        imattrib = list(data.attrib[inds])
-        imorig = list(data.orig[inds])
-        imsource.data.update({
-            'attrib': imattrib,
-            'orig': imorig,
-            'x': (np.arange(len(inds), dtype=int)*wid)%maxwid,
-            'y': (np.arange(len(inds), dtype=int)*wid)//maxwid*hei,
+        stats.view = CDSView(source=sample_src, filters=[IndexFilter(new)])
+        inds = sample_src.selected.indices[:num] if len(sample_src.selected.indices) else list(range(num))
+
+        image_src.data.update({
+            'attribution': list(data.attribution[data.index[inds]]),
+            'original'  : list(original_loader[data.index[inds]]),
+            'x'     : (np.arange(len(inds), dtype=int)*wid)%maxwid,
+            'y'     : (np.arange(len(inds), dtype=int)*wid)//maxwid*hei,
         })
 
     def update_clusters(attr, old, new):
-        source.data.update({'label': data.label[new]})
-        data.lab = new
+        data.sel.clu = new
+        sample_src.data.update({'cluster': data.cluster[data.sel.clu]})
 
     def update_alpha(attr, old, new):
-        imorig.glyph.global_alpha=1.-new
-        imattr.glyph.global_alpha=new
+        attrib_rend.glyph.global_alpha=1.-new
+        original_rend.glyph.global_alpha=new
 
-    source.selected.on_change('indices', update_selection)
-    radio.on_change('active', update_clusters)
-    slider.on_change('value', update_alpha)
-    select.on_change('value', update_cat)
+    # === Event Registration ===
+    category_select.on_change('value', update_cat)
+    cluster_select.on_change('value', update_clusters)
+    alpha_slider.on_change('value', update_alpha)
+    sample_src.selected.on_change('indices', update_selection)
 

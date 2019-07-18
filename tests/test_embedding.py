7397f3479cfc6c37bc5bf477916ccccac8a8f956
Talmaj Marinc <talmaj.marinc@hhi.fraunhofer.de>
2019-07-18 10:27:36 +0200

    Add test that computes embeddings based on precomputed distances.

--- tests/test_embedding.py
+++ tests/test_embedding.py
@@ -27,3 +27,10 @@ def test_eigen_decomposition(distances):
     eigval, eigvec = embedding.EigenDecomposition(n_eigval=32)(distances)
     assert eigval.shape == (32, )
     assert eigvec.shape == (1797, 32)
+
+
+@pytest.mark.parametrize('processor', [embedding.TSNEEmbedding, embedding.LLEEmbedding, embedding.PCAEmbedding,
+                                       embedding.UMAPEmbedding])
+def test_embedding_on_distances(processor, distances):
+    emb = processor(metric='precomputed')(distances)
+    assert emb.shape == (1797, 2)

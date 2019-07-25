79d6f6f21221f43c69114bf36bef62445f3a165f
chrstphr <chrstphr@posteo.eu>
2019-07-25 13:20:48 +0200

    added Param tests

--- tests/sprincl/processor/test_base.py
+++ tests/sprincl/processor/test_base.py
@@ -36,6 +36,25 @@ def function():
         return 42
     return some_function
 
+class TestParam(object):
+    def test_instatiation(self):
+        Param(object)
+
+    def test_dtype_not_assigned(self):
+        with pytest.raises(TypeError):
+            Param()
+
+    def test_dtype_no_type(self):
+        with pytest.raises(TypeError):
+            Param('monkey')
+
+    def test_dtype_multiple(self):
+        param = Param((object, type))
+        assert param.dtype == (object, type)
+
+    def test_dtype_single_to_tuple(self):
+        param = Param(object)
+        assert param.dtype == (object,)
 
 class TestProcessor(object):
     def test_params_tracked(self, processor_type):

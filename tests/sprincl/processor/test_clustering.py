55383b354b294a6a7a76d1255531b2a378c84576
Talmaj Marinc <talmaj.marinc@hhi.fraunhofer.de>
2019-07-31 11:09:42 +0200

    Fix and optimize processor tests.

--- tests/sprincl/processor/test_clustering.py
+++ tests/sprincl/processor/test_clustering.py
@@ -7,6 +7,12 @@ from sklearn.metrics.pairwise import euclidean_distances
 
 from sprincl.processor import clustering
 
+try:
+    import hdbscan  # noqa: F401
+    extra_processors = [clustering.HDBSCAN]
+except ImportError:
+    extra_processors = []
+
 
 @pytest.fixture
 def data():
@@ -15,24 +21,30 @@ def data():
 
 
 @pytest.fixture
+def tiny_data():
+    data, _ = make_blobs(50, centers=5, random_state=100)
+    return data
+
+
+@pytest.fixture
 def distances(data):
     return euclidean_distances(data)
 
 
-@pytest.mark.parametrize('processor', [clustering.KMeans, clustering.AgglomerativeClustering, clustering.DBSCAN,
-                                       clustering.HDBSCAN])
+@pytest.mark.parametrize('processor', [clustering.AgglomerativeClustering, clustering.KMeans])
 def test_clustering(processor, data):
-    # TODO DBSCAN and HDBSCAN are failing this test, not sure how to use them
     emb = processor(n_clusters=5)(data)
     # since blobs are equal the cluster sizes should be approximately the same size
+    assert len(np.unique(emb)) == 5
     assert (np.unique(emb, return_counts=True)[1] / 1000).std() < 0.05
 
 
-@pytest.mark.parametrize('processor', [clustering.KMeans, clustering.DBSCAN, clustering.HDBSCAN])
+@pytest.mark.parametrize('processor', [clustering.DBSCAN] + extra_processors)
 def test_embedding_on_distances(processor, distances):
-    # TODO DBSCAN, HDBSCAN are failing this test. Any suggestions?
-    emb = processor(n_clusters=5, metric='precomputed')(distances)
-    assert (np.unique(emb, return_counts=True)[1] / 1000).std() < 0.05
+    params = {'eps': 0.9} if 'eps' in processor.params else {}
+    emb = processor(metric='precomputed', **params)(distances)
+    assert len(np.unique(emb)) == 5
+    assert (np.unique(emb, return_counts=True)[1] / 1000).std() < 0.13
 
 
 def test_embedding_on_distances_agg_clustering(distances):
@@ -40,18 +52,18 @@ def test_embedding_on_distances_agg_clustering(distances):
     assert (np.unique(emb, return_counts=True)[1] / 1000).std() < 0.05
 
 
-def test_denrogram_creation(data):
+def test_denrogram_creation(tiny_data):
     output_path = '/tmp/dendrogram.png'
-    data2 = clustering.Dendrogram(output_file=output_path)(data)
-    np.testing.assert_equal(data, data2)
+    data2 = clustering.Dendrogram(output_file=output_path)(tiny_data)
+    np.testing.assert_equal(tiny_data, data2)
     assert os.path.exists(output_path)
     os.remove(output_path)
 
 
-def test_denrogram_creation_with_file_object(data):
+def test_denrogram_creation_with_file_object(tiny_data):
     output_path = '/tmp/dendrogram.png'
     with open(output_path, 'wb') as f:
-        data2 = clustering.Dendrogram(output_file=f)(data)
-        np.testing.assert_equal(data, data2)
+        data2 = clustering.Dendrogram(output_file=f)(tiny_data)
+        np.testing.assert_equal(tiny_data, data2)
         assert os.path.exists(output_path)
     os.remove(output_path)
--- tests/sprincl/processor/test_embedding.py
+++ tests/sprincl/processor/test_embedding.py
@@ -4,10 +4,18 @@ from sklearn.metrics.pairwise import euclidean_distances
 
 from sprincl.processor import embedding
 
+embedding_processors = [embedding.TSNEEmbedding, embedding.LLEEmbedding, embedding.PCAEmbedding]
+
+try:
+    import umap  # noqa: F401
+    extra_processors = [embedding.UMAPEmbedding]
+except ImportError:
+    extra_processors = []
+
 
 @pytest.fixture
 def data():
-    digits = load_digits()  # shape 1797 x 64
+    digits = load_digits(2)  # shape 1797 x 64
     return digits.data
 
 
@@ -16,21 +24,19 @@ def distances(data):
     return euclidean_distances(data)
 
 
-@pytest.mark.parametrize('processor', [embedding.TSNEEmbedding, embedding.LLEEmbedding, embedding.PCAEmbedding,
-                                       embedding.UMAPEmbedding])
+@pytest.mark.parametrize('processor', embedding_processors + extra_processors)
 def test_embedding(processor, data):
     emb = processor()(data)
-    assert emb.shape == (1797, 2)
+    assert emb.shape == (360, 2)
 
 
 def test_eigen_decomposition(distances):
     eigval, eigvec = embedding.EigenDecomposition(n_eigval=32)(distances)
     assert eigval.shape == (32, )
-    assert eigvec.shape == (1797, 32)
+    assert eigvec.shape == (360, 32)
 
 
-@pytest.mark.parametrize('processor', [embedding.TSNEEmbedding, embedding.LLEEmbedding, embedding.PCAEmbedding,
-                                       embedding.UMAPEmbedding])
+@pytest.mark.parametrize('processor', [embedding.TSNEEmbedding] + extra_processors)
 def test_embedding_on_distances(processor, distances):
     emb = processor(metric='precomputed')(distances)
-    assert emb.shape == (1797, 2)
+    assert emb.shape == (360, 2)

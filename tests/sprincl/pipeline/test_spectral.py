39ef50723f2380e8af327ed47ad76bb16ee7eb9c
Sebastian Lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2019-08-02 08:48:20 +0200

    committing some WIP stuff. keep away.

--- tests/sprincl/pipeline/test_spectral.py
+++ tests/sprincl/pipeline/test_spectral.py
@@ -13,7 +13,7 @@ from sprincl.pipeline.base import Task
 from sprincl.pipeline.spectral import SpectralEmbedding, SpectralClustering
 
 from sprincl.processor.affinity import SparseKNN
-from sprinc.processor.laplacian import SymmetricNormalLaplacian
+from sprincl.processor.laplacian import SymmetricNormalLaplacian
 from sprincl.processor.embedding import EigenDecomposition
 from sprincl.processor.clustering import KMeans
 
@@ -112,3 +112,116 @@ class TestSpectral(object):
             .format(spiral_data.shape[0], labels.size)
 
         assert labels.ndim == 1, 'Expected labels to be flat array, but was shaped {}'.format(labels.shape)
+
+
+
+    def test_spectral_clustering_step_by_step_custom_params(self, spiral_data, k_knn, k_eig, k_clusters):
+        # this test manually compares the pipelines working order against manually
+        # executed steps with custom parameters attuned to the spiral data
+
+        # first, separately prepare processors for customized pipeline
+        # customizations:
+        #   1) parameters, as passed to the test function
+        #   2) all processors (affected by changed parameters) produce outputs this time for later comparison
+        knn = SparseKNN(k_neighbors=k_knn, symmetric=True, is_output=True)
+        lap = #CONTINUE HERE: 1) BACKUP THIS CODE; GO TO / FAST FORWARD MASTER / CONTINUE WRITING TEST CODE.
+
+        assert isinstance(knn, Processor), 'YAY'
+        pipeline = SpectralClustering(
+            affinity=knn,
+
+        )
+
+        output = pipeline(spiral_data)
+        assert len(output) == 2, 'len output is {}'.format(len(output))
+
+
+        # WEIRDNESS BELOW!
+
+        # pipeline = SpectralClustering(
+        #    affinity=SparseKNN(k_neighbors=k_knn, symmetric=True),
+        #    embedding=EigenDecomposition(n_eigval=k_eig),
+        #    clustering=KMeans(n_clusters=k_clusters) # WORKS!
+        # )
+
+        # pipeline = SpectralClustering(
+        #     affinity=Task(default=SparseKNN(k_neighbors=k_knn, symmetric=True)), #<- change!
+        #     embedding=EigenDecomposition(n_eigval=k_eig),
+        #     clustering=KMeans(n_clusters=k_clusters) #fails because of this!
+        # )
+
+
+
+        # def test_assert_with_spiral_data(self):
+        #     # this test tries to run/assert the results of the Pipeline implementation with a manual pipeline.
+
+
+        #     from sprincl.pipeline.spectral import SpectralClustering
+
+
+
+        #     def knn(distance, k, symmetric):
+        #         """Compute Sparse K-Nearest-Neighbors affinity matrix.
+        #         Note: This is the old KNN code from previous sprincl versions
+
+        #         Parameters
+        #         ----------
+        #         distance : :obj:`numpy.ndarray`
+        #             Distance matrix used to compute affinity matrix.
+
+        #         Returns
+        #         ------
+        #         :obj:`sp.csr_matrix`
+        #             Sparse CSR representation of KNN affinity matrix
+
+        #         """
+        #         # number of samples
+        #         n = distance.shape[0]
+
+        #         # silently use maximum number of neighbors if there are more samples than k
+        #         k = k if k < (n-1) else (n-1)
+
+        #         # set up indices for sparse representation of nearest neighbors
+        #         cols = distance.argsort(1)[:, 1:k+1]
+        #         rows = np.mgrid[:n, :k][0]
+        #         # existing edges are denoted with ones
+        #         vals = np.ones((n, k), dtype=distance.dtype)
+        #         affinity = sp.csr_matrix((vals.flat, (rows.flat, cols.flat)), shape=(n, n))
+
+        #         # make the affinity matrix symmetric
+        #         if symmetric:
+        #             affinity = (affinity + affinity.T) / 2.
+        #         return affinity
+
+        #     def laplacian(affinity):
+        #         """ Note: this is the old laplacian code from a previous sprincl version """
+        #         def A1ifmat(x):
+        #             return x.A1 if isinstance(x, np.matrix) else x
+        #         deg = sp.diags(A1ifmat(affinity.sum(1))**-.5, 0)
+        #         lap = deg @ affinity @ deg
+        #         return lap
+
+        #     #generate some data
+        #     np.random.seed(1345123)
+        #     data = spiral_data(150)
+
+        #     #run the manual pipeline
+        #     k_knn = int(np.log(data.shape[0]))
+        #     k_eig = 8
+        #     k_clusters = 4
+
+        #     D_man = squareform(pdist(data))
+        #     A_man = knn(D_man, k=k_knn, symmetric=True)
+        #     L_man = laplacian(A_man)
+
+        #     eigval_man, eigvec_man = eigsh(L_man, k=k_eig, which='LM')
+        #     eigval_man = 1. - eigval_man
+        #     eigvec_man /= np.linalg.norm(eigvec_man, axis=1, keepdims=True)
+        #     labels_man = KMeans(n_clusters=k_clusters).fit_predict(eigvec_man)
+        #     e_data_man = TSNE().fit_transform(eigvec_man)   #compute embedding for Data
+
+        #     # build the SpRAy pipeline
+
+        #     SC = SpectralClustering
+        #     labels_pipe = SC(data)
+        #     print(labels_pipe)

76e8034240cc04d74cf2039ca95a6771c5ce1133
chrstphr <chrstphr@posteo.eu>
2021-07-05 14:28:55 +0200

    Prepare setup.py for PyPI upload

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_packages
 
+
+with open('README.md', 'r', encoding='utf-8') as fd:
+    long_description = fd.read()
+
+
 setup(
     name='virelay',
     use_scm_version=True,
-    packages=['virelay'],
+    author='chrstphr',
+    author_email='virelay@j0d.de',
+    description='Web-app to view source-data, attributions, clusterings, and (t-SNE-)embeddings.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/virelay/virelay',
+    packages=find_packages(include=['virelay*']),
     install_requires=[
         'h5py>=2.10.0',
         'matplotlib>=3.2.2',
@@ -32,5 +43,13 @@ setup(
             'frontend/distribution/assets/images/*.png',
             'frontend/distribution/3rdpartylicenses.txt'
         ]
-    }
+    },
+    python_requires='>=3.7',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ]
 )

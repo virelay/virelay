9f047e764cf3cc10f7dd7b70ad654353741857b0
David Neumann <david.neumann@lecode.de>
2022-04-04 14:13:35 +0200

    Fixed all linting warnings of pylint and flake8.

--- virelay/__main__.py
+++ virelay/__main__.py
@@ -2,9 +2,5 @@
 
 from .application import Application
 
-def main():
-    APPLICATION = Application()
-    APPLICATION.run()
-
-if __name__ == '__main__':
-    main()
+APPLICATION = Application()
+APPLICATION.run()
